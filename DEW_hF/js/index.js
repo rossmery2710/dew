d1009ae02d4208c9aa472101a3e58d9240aae963
rossmery2710 <44813374+rossmery2710@users.noreply.github.com>
2018-11-06 11:43:02 -0500

    Add files via upload

---
+++ DEW_hF/js/index.js
@@ -0,0 +1,9 @@
+var url =["www.facebook.com","www.instagram.com","www.twitter.com","www.whatsapp.com"];
+function cambiarRedes(){
+
+	var redes = document.getElementsByClassName("gruporedes");
+	for( var i = 0; i < url.length; i++){
+		
+		redes[i].onmouseover ;
+	}
+}// JavaScript Document
